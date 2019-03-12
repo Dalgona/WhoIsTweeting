1b22beab207d327be0cd1ae983f4f94744d61249
Dalgona <dalgona@hontou.moe>
2019-03-12 10:30:20 +0900

    Separate LastTweetConverter class

---
+++ WPFWhoIsTweeting/LastTweetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WhoIsTweeting
+{
+    public class LastTweetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            UserListItem item = value as UserListItem;
+            if (item?.LastTweet < new DateTime(2002, 1, 1)) return "";
+
+            int? minutes = item?.MinutesFromLastTweet;
+            if (minutes <= 5) return "";
+            else if (minutes <= 15) return $"{minutes} min";
+            else return minutes <= 1440 ? item?.LastTweet.ToString("HH:mm") : item?.LastTweet.ToString("yy/MM/dd");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => null;
+    }
+}
--- WPFWhoIsTweeting/vm/MainViewModel.cs
+++ WPFWhoIsTweeting/vm/MainViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Globalization;
 using System.Threading;
 using System.Windows;
 using System.Windows.Data;
@@ -240,22 +239,5 @@ namespace WhoIsTweeting
             GC.SuppressFinalize(this);
         }
         #endregion
-    }
-
-    public class LastTweetConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            UserListItem item = value as UserListItem;
-            if (item?.LastTweet < new DateTime(2002, 1, 1)) return "";
-
-            int? minutes = item?.MinutesFromLastTweet;
-            if (minutes <= 5) return "";
-            else if (minutes <= 15) return $"{minutes} min";
-            else return minutes <= 1440 ? item?.LastTweet.ToString("HH:mm") : item?.LastTweet.ToString("yy/MM/dd");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => null;
     }
 }
