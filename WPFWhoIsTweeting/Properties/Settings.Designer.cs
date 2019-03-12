bb371ac7cb32e3727a58251f731f0812fcc5c894
Dalgona <dalgona@hontou.moe>
2019-03-12 17:56:30 +0900

    Remove moved codes from original project

--- WPFWhoIsTweeting/Properties/Settings.Designer.cs
+++ WPFWhoIsTweeting/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WhoIsTweeting.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,54 +20,6 @@ namespace WhoIsTweeting.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string ConsumerKey {
-            get {
-                return ((string)(this["ConsumerKey"]));
-            }
-            set {
-                this["ConsumerKey"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string ConsumerSecret {
-            get {
-                return ((string)(this["ConsumerSecret"]));
-            }
-            set {
-                this["ConsumerSecret"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Token {
-            get {
-                return ((string)(this["Token"]));
-            }
-            set {
-                this["Token"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string TokenSecret {
-            get {
-                return ((string)(this["TokenSecret"]));
-            }
-            set {
-                this["TokenSecret"] = value;
             }
         }
         
@@ -116,18 +68,6 @@ namespace WhoIsTweeting.Properties {
             }
             set {
                 this["UpdateSettings"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("30")]
-        public int UpdateInterval {
-            get {
-                return ((int)(this["UpdateInterval"]));
-            }
-            set {
-                this["UpdateInterval"] = value;
             }
         }
     }
