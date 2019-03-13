3be725ae11e9b03f5f6277c6c6b17fe489eeb58b
Dalgona <dalgona@hontou.moe>
2019-03-13 21:43:37 +0900

    Create Wit.UI.Core project
    Add ViewModelBase abstrct class

---
+++ Wit.UI.Core/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Wit.UI.Core
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
