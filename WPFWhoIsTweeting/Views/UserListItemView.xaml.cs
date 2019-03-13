40942aeec274ae5f748c8c340a950e04b8b9c657
Dalgona <dalgona@hontou.moe>
2019-03-13 23:42:20 +0900

    Extract UserListItemView

---
+++ WPFWhoIsTweeting/Views/UserListItemView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WhoIsTweeting.Views
+{
+    public partial class UserListItemView : UserControl
+    {
+        public UserListItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
