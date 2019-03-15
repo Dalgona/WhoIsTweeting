4fff557bacf0f7f41a8f1b51fcc00d77a54364ea
Dalgona <dalgona@hontou.moe>
2019-03-15 16:16:41 +0900

    Add StatData class

---
+++ Wit.Core/StatData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wit.Core
+{
+    public class StatData
+    {
+        public DateTime Date { get; }
+        public int OnlineCount { get; }
+        public int AwayCount { get; }
+        public int OfflineCount { get; }
+
+        public StatData(int online, int away, int offline)
+        {
+            Date = DateTime.Now;
+            OnlineCount = online;
+            AwayCount = away;
+            OfflineCount = offline;
+        }
+    }
+}
