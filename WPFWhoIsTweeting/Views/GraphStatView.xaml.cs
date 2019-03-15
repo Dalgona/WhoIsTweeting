5ce9de80d41fa21aa693601db05ae85c24985b20
Dalgona <dalgona@hontou.moe>
2019-03-15 16:48:31 +0900

    Add GraphStatView

---
+++ WPFWhoIsTweeting/Views/GraphStatView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WhoIsTweeting.Views
+{
+    public partial class GraphStatView : UserControl
+    {
+        public GraphStatView()
+        {
+            InitializeComponent();
+        }
+    }
+}
