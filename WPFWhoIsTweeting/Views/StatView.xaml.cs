b26a548ee52f6bb639e61dcf51d6762e302b879f
Dalgona <dalgona@hontou.moe>
2019-03-15 15:16:17 +0900

    Replace polygon graph with a histogram
    TODO: Reimplement zoom and peek

--- WPFWhoIsTweeting/Views/StatView.xaml.cs
+++ WPFWhoIsTweeting/Views/StatView.xaml.cs
@@ -6,8 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Threading;
 using Microsoft.Win32;
 using Wit.Core;
 using Wit.VM;
@@ -19,92 +17,11 @@ namespace WhoIsTweeting.Views
         private StatViewModel viewModel;
         private MainService service = MainService.Instance;
 
-        private DispatcherTimer timer;
-        private int dataIndex;
-
-        private double graphScale = 1.0;
-
         public StatView()
         {
             InitializeComponent();
 
             DataContext = viewModel = new StatViewModel();
-            viewModel.PropertyChanged += ViewModel_PropertyChanged;
-
-            timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(500) };
-            timer.Tick += Timer_Tick;
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            timer.Stop();
-            peek.DataContext = service.Graph[dataIndex == 0 ? 0 : dataIndex - 1];
-            peek.IsOpen = true;
-        }
-
-        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
-            => Dispatcher.Invoke(drawGraph);
-
-        private void drawGraph()
-        {
-            int num = viewModel.DataCount;
-            double followings = viewModel.FollowingsCount;
-            var pt = polyAway.Points;
-            pt.Clear();
-            pt.Add(new Point(0, 1));
-            for (int i = 0; i < num; i++)
-            {
-                double x = (i + 1.0) / num;
-                double y = 1 - ((service.Graph[i].Value[0] + service.Graph[i].Value[1]) / followings) * graphScale;
-                pt.Add(new Point(x, y));
-            }
-            pt.Add(new Point(1, 1));
-            pt = polyOnline.Points;
-            pt.Clear();
-            pt.Add(new Point(0, 1));
-            for (int i = 0; i < num; i++)
-            {
-                double x = (i + 1.0) / num;
-                double y = 1 - (service.Graph[i].Value[0] / followings) * graphScale;
-                pt.Add(new Point(x, y));
-            }
-            pt.Add(new Point(1, 1));
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            drawGraph();
-        }
-
-        private void MoveCursor(object sender, MouseEventArgs e)
-        {
-            if (viewModel.DataCount == 0) return;
-            timer.Stop();
-            peek.IsOpen = false;
-            Point pos = e.GetPosition(graphGrid);
-            double segWidth = graphGrid.ActualWidth / viewModel.DataCount;
-            dataIndex = (int)Math.Round(pos.X / segWidth);
-            cursor.X1 = cursor.X2 = Math.Floor(dataIndex * segWidth) - 0.5;
-            timer.Start();
-        }
-
-        private void graphGrid_MouseEnter(object sender, MouseEventArgs e)
-        { 
-            cursor.Visibility = Visibility.Visible;
-        }
-
-        private void graphGrid_MouseLeave(object sender, MouseEventArgs e)
-        { 
-            cursor.Visibility = Visibility.Hidden;
-            peek.IsOpen = false;
-            timer.Stop();
-        }
-
-        private void graphGrid_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (e.Delta > 0 && graphScale <= 5.0) graphScale += 0.1;
-            if (e.Delta < 0 && graphScale >= 1.0) graphScale -= 0.1;
-            drawGraph();
         }
 
         private void OnResetClick(object sender, RoutedEventArgs e)
