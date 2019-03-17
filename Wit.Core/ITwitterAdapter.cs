5abfc25e5308d0cba21f5ff0ffec3cc19a6c860d
Dalgona <dalgona@hontou.moe>
2019-03-17 19:54:42 +0900

    [WIP] Add ITwitterAdapter and its implementation

---
+++ Wit.Core/ITwitterAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Wit.Core
+{
+    interface ITwitterAdapter
+    {
+        Task<TwitterApiResult<string>> CheckUser();
+
+        Task<TwitterApiResult<IEnumerable<string>>> RetrieveFollowingIds(string userId);
+    }
+}
---
+++ Wit.Core/TwitterAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using PicoBird;
+using PicoBird.Objects;
+
+namespace Wit.Core
+{
+    class TwitterAdapter : ITwitterAdapter
+    {
+        API _api = new API("", "");
+
+        public async Task<TwitterApiResult<string>> CheckUser()
+        {
+            try
+            {
+                User user = await _api.Get<User>("/1.1/account/verify_credentials.json");
+
+                return user.id_str;
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
+
+        public async Task<TwitterApiResult<IEnumerable<string>>> RetrieveFollowingIds(string userId)
+        {
+            try
+            {
+                CursoredIdStrings ids =
+                    await _api.Get<CursoredIdStrings>(
+                        "/1.1/friends/ids.json",
+                        new NameValueCollection
+                        {
+                            { "user_id", userId },
+                            { "stringify_id", "true" },
+                            { "count", "5000" }
+                        }
+                    );
+
+                return ids.ids;
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
+    }
+}
