a567d19f0f50a78be2f195e4f5b249cc20db5c5d
Dalgona <dalgona@hontou.moe>
2019-04-06 13:47:44 +0900

    Separate ErrorMessageView

---
+++ WPFWhoIsTweeting/Views/ErrorMessageView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Wit.Core;
+
+namespace WhoIsTweeting.Views
+{
+    public partial class ErrorMessageView : UserControl
+    {
+        public static readonly DependencyProperty AuthStatusProperty =
+            DependencyProperty.Register(
+                nameof(AuthStatus),
+                typeof(AuthStatus),
+                typeof(ErrorMessageView),
+                new UIPropertyMetadata(AuthStatus.NeedConsumerKey)
+            );
+
+        public static readonly DependencyProperty LastErrorProperty =
+            DependencyProperty.Register(
+                nameof(LastError),
+                typeof(TwitterErrorType),
+                typeof(ErrorMessageView),
+                new UIPropertyMetadata(TwitterErrorType.None)
+            );
+
+        public AuthStatus AuthStatus
+        {
+            get => (AuthStatus)GetValue(AuthStatusProperty);
+            set => SetValue(AuthStatusProperty, value);
+        }
+
+        public TwitterErrorType LastError
+        {
+            get => (TwitterErrorType)GetValue(LastErrorProperty);
+            set => SetValue(LastErrorProperty, value);
+        }
+
+        public ErrorMessageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
